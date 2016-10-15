74d8d19174a7fec905e84da4f6ffa59fbf83ad89
Syrus Akbary <me@syrusakbary.com>
2016-10-15 15:04:46 -0700

    Added test issues. Fixed #11

---
+++ tests/test_issues.py
@@ -0,0 +1,19 @@
+# This tests reported issues in the Promise package
+from promise import Promise
+
+
+def test_issue_11():
+    # https://github.com/syrusakbary/promise/issues/11
+    def test(x):                                                                
+        def my(resolve, reject):                                                  
+            if x > 0:                                                               
+                resolve(x)                                                            
+            else:                                                                   
+                reject(Exception(x))                                                            
+        return Promise(my)                                                        
+
+    promise_resolved = test(42).then(lambda x: x)
+    assert promise_resolved.value == 42
+
+    promise_rejected = test(-42).then(lambda x: x, lambda e: str(e))
+    assert promise_rejected.value == "-42"
