732c2fd7f7fdd4d7edc7063fbfb774077c4b7fc5
Syrus Akbary <me@syrusakbary.com>
2017-03-10 23:42:43 -0800

    Added extra dataloader test

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -334,12 +334,36 @@ def test_caches_failed_fetches():
 #     assert load_calls == [['A', 'B']]
 
 
+@Promise.safe
+def test_can_call_a_loader_from_a_loader():
+    deep_loader, deep_load_calls = id_loader()
+    def do_resolve(x):
+        raise Exception("AOH!")
+    a_loader, a_load_calls = id_loader(resolve=do_resolve)
+    b_loader, b_load_calls = id_loader(resolve=lambda keys:deep_loader.load(list(keys)))
+
+    a1, b1, a2, b2 = Promise.all([
+        a_loader.load('A1'),
+        b_loader.load('B1'),
+        a_loader.load('A2'),
+        b_loader.load('B2')
+    ]).get()
+
+    assert a1 == 'A1'
+    assert b1 == 'B1'
+    assert a2 == 'A2'
+    assert b2 == 'B2'
+
+    assert a_load_calls == [['A1', 'A2']]
+    assert b_load_calls == [['B1', 'B2']]
+    assert deep_load_calls == [[['A1', 'A2'], ['B1', 'B2']]]
+
+
 # @Promise.safe
 # def test_can_call_a_loader_from_a_loader():
 #     deep_loader, deep_load_calls = id_loader()
 #     a_loader, a_load_calls = id_loader(resolve=lambda keys:deep_loader.load(tuple(keys)))
 #     b_loader, b_load_calls = id_loader(resolve=lambda keys:deep_loader.load(tuple(keys)))
-
 
 #     a1, b1, a2, b2 = Promise.all([
 #         a_loader.load('A1'),
@@ -355,4 +379,4 @@ def test_caches_failed_fetches():
 
 #     assert a_load_calls == [['A1', 'A2']]
 #     assert b_load_calls == [['B1', 'B2']]
-#     assert deep_load_calls == [[['A1', 'A2'], ['B1', 'B2']]]
+#     assert deep_load_calls == [[('A1', 'A2'), ('B1', 'B2')]]
