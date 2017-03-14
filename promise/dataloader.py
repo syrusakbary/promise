5618c8070ab195af377b4e0f9a00103a34dc16d6
Syrus Akbary <me@syrusakbary.com>
2017-03-14 00:50:59 -0700

    Removed unused identity function

--- promise/dataloader.py
+++ promise/dataloader.py
@@ -7,10 +7,6 @@ from .promise import Promise, async_instance
 from .context import Context
 
 
-def identity(x):
-    return x
-
-
 def get_chunks(iterable_obj, chunk_size=1):
     chunk_size = max(1, chunk_size)
     return (iterable_obj[i:i + chunk_size] for i in range(0, len(iterable_obj), chunk_size))
